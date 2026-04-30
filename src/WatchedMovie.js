a36311030ce0fcce38e988d14167c0f9a800581c
You <you@example.com>
2026-05-01 02:11:21 +0300

    Add src/WatchedMovie.js

---
+++ src/WatchedMovie.js
@@ -0,0 +1,26 @@
+// WatchedMovie Component
+export default function WatchedMovie({ movie, onDelete }) {
+  return (
+    <li>
+      <img src={movie.poster} alt={`${movie.title} poster`} />
+      <h3>{movie.title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+        <button className="btn-delete" onClick={() => onDelete(movie.imdbID)}>
+          &#10005;
+        </button>
+      </div>
+    </li>
+  );
+}
