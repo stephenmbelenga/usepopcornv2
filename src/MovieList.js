224a4a70e2b08784af473e6c9294dc341b5bc2e8
You <you@example.com>
2026-05-01 02:11:20 +0300

    Add src/MovieList.js

---
+++ src/MovieList.js
@@ -0,0 +1,12 @@
+// MovieList Component
+import Movie from "./Movie";
+
+export default function MovieList({ movies, onSelectMovie }) {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
+      ))}
+    </ul>
+  );
+}
